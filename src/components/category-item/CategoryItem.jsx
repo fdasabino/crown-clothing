f1463c4661d1849457cc3e4bc961875c56d2a428
Francisco Sabino <fdasabino@gmail.com>
2022-07-11 07:48:47 +0200

    updating category item component to separate comp.

---
+++ src/components/category-item/CategoryItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { categories } from "../../data";
+import "./CategoryItem.scss";
+
+const Categories = () => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <div key={category.id} className="category-container">
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${category.imageUrl})` }}
+          />
+          <div className="category-body-container" key={category.id}>
+            <h2>{category.title}</h2>
+            <p>Shop</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CategoryItem from "../components/category-item/CategoryItem";
+
+const Home = () => {
+  return (
+    <div>
+      <CategoryItem />
+    </div>
+  );
+};
+
+export default Home;
