16626c8ccf89e2fd7ab00dc08440b5a7c5696259
Francisco Sabino <fdasabino@gmail.com>
2022-09-07 22:37:27 +0200

    added redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import { legacy_createStore as createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { composeWithDevTools } from "redux-devtools-extension";
-
 import { rootReducer } from "./rootReducer";
 
 const composedEnhancers = composeWithDevTools();
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
