7fa40f1b5894a3a8d5f4363030fa228161a14655
Francisco Sabino <fdasabino@gmail.com>
2022-10-03 19:30:11 +0200

    typed user redux

---
+++ src/store/user/userReducer.ts
@@ -0,0 +1,30 @@
+import { USER_ACTION_TYPES } from "./userActionTypes";
+import { AnyAction } from "redux";
+import { signInFailed, signUpFailed, signOutFailed, signOutSuccess, signInSuccess } from "./userAction";
+import { UserData } from "../../utils/firebase";
+
+export type UserState = {
+  readonly currentUser: UserData | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (state = INITIAL_STATE, action: AnyAction) => {
+  if (signInSuccess.match(action)) {
+    return { ...state, currentUser: action.payload };
+  }
+  if (signOutSuccess.match(action)) {
+    return { ...state, currentUser: null };
+  }
+  if (signInFailed.match(action) || signUpFailed.match(action) || signOutFailed.match(action)) {
+    return { ...state, error: action.payload };
+  }
+
+  return state;
+};
